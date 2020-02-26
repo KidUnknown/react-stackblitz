cffa29ad832a48de9f345d87b36b826dce33abfd
KidUnknown <jonathan_theobald@dennis.co.uk>
2020-02-26 11:45:56 +0000

    1 file modified with body content

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import Title from './Title';
 import './style.css';
-
+/*                                                                            */
 class App extends Component {
   constructor() {
     super();
     this.state = {
       name: 'React',
-      title: 'My first stackblitz app'
+      title: 'My first stackblitz app',
+      bodycopy: 'Just a small sentence for the body area'
     };
   }
 
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
         <Title title={this.state.title} />
+        <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          {this.state.bodycopy}
         </p>
       </div>
     );
