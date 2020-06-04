10a146ab941828f6c0de70070b16c77cfaa19b88
KidUnknown <jonathan_theobald@dennis.co.uk>
2020-06-04 15:46:32 +0100

    code tidy with destructuring

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ import './style.css';
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
+
+    // props = {
+    //   headerProps: {
+    //     HeaderTitle: '',
+    //     HeaderRole: '',
+    //     HeaderClass: ''
+    //   },
+    //   footerProps: {
+    //     FooterTitle: '',
+    //     FooterClass: ''
+    //   }
+    // };
+    
     this.state = {
       error: null,
       isLoaded: false,
@@ -14,43 +27,39 @@ class MyComponent extends React.Component {
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/photos")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      }
+    )
+    .catch((error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    })
   }
 
   render() {
     const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>
-              {item.id} {item.albumId} {item.thumbnailUrl}
-            </li>
-          ))}
-        </ul>
-      );
-    }
+
+    if (error) return <div>Error: {error.message}</div>;
+    if (!isLoaded) return <div>Loading...</div>;
+
+    return (
+      <ul>
+        {items.map(item => {
+          const {albumId,id,thumbnailUrl} = item;
+          <li key={id}>
+            <img id={id} alt={albumId} src={thumbnailUrl} />
+          </li>
+        })}
+      </ul>
+    );
   }
 }
 
